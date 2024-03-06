f27532d3b6870dbdca7a5a099fb59d27221150c5
Mau Fournier <mau.fournier@gmail.com>
2024-03-06 17:15:03 -0600

    feat: Add stubs for name input

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-      <h1>Gumroad email marketing prototype</h1>
+      <label htmlFor="name">Name:</label>
+      <div>
+        <input type="text" id="name" />
+      </div>
     </>
   )
 }
