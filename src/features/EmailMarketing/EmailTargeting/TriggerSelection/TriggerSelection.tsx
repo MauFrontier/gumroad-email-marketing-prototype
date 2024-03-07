5bc6eda4ed7587fea5236b6ba77d37167daf5500
MauFrontier <maufrontier@gmail.com>
2024-03-06 21:54:47 -0600

    feat: Add sample trigger data

--- src/features/EmailMarketing/EmailTargeting/TriggerSelection/TriggerSelection.tsx
+++ src/features/EmailMarketing/EmailTargeting/TriggerSelection/TriggerSelection.tsx
@@ -1,5 +1,28 @@
 import Sticker from '../../../shared/ui/Sticker/Sticker';
 
+const triggers = [
+  {
+    stickerURI: './src/assets/images/stickers/sticker_thumbs-up.svg',
+    name: 'Purchase',
+    shortDescription: 'Purchase',
+  },
+  {
+    stickerURI: './src/assets/images/stickers/sticker_click.svg',
+    name: 'Cancellation',
+    shortDescription: 'Cancellation',
+  },
+  {
+    stickerURI: './src/assets/images/stickers/sticker_peace.svg',
+    name: 'Refund',
+    shortDescription: 'Refund',
+  },
+  {
+    stickerURI: './src/assets/images/stickers/sticker_talking-mouth.svg',
+    name: 'Refund',
+    shortDescription: 'Refund',
+  },
+];
+
 const TriggerSelection = () => {
   return (
     <div data-testid="TriggerSelection">
