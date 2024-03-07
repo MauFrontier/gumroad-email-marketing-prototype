9fbb9fec8672d40a3eda872156530942a93bbe53
Mau Fournier <mau.fournier@gmail.com>
2024-03-06 19:09:03 -0600

    chore: Use "it", not "test", for consistency

--- src/features/EmailMarketing/EmailTargeting/EmailTargeting.test.tsx
+++ src/features/EmailMarketing/EmailTargeting/EmailTargeting.test.tsx
@@ -2,7 +2,7 @@ import {render, screen} from '@testing-library/react';
 import EmailTargeting from './EmailTargeting';
 
 describe('EmailTargeting additional tests', () => {
-  test('renders component', () => {
+  it('renders component', () => {
     render(<EmailTargeting />);
     expect(screen.getByTestId('EmailTargeting')).toBeInTheDocument();
   });
