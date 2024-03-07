74e1de07a9088b6098321c2392798a1ce2c98f39
MauFrontier <maufrontier@gmail.com>
2024-03-06 21:53:30 -0600

    feat: Display a Sticker

--- src/features/EmailMarketing/EmailTargeting/TriggerSelection/TriggerSelection.tsx
+++ src/features/EmailMarketing/EmailTargeting/TriggerSelection/TriggerSelection.tsx
@@ -1,5 +1,14 @@
+import Sticker from '../../../shared/ui/Sticker/Sticker';
+
 const TriggerSelection = () => {
-  return <div data-testid="TriggerSelection"></div>;
+  return (
+    <div data-testid="TriggerSelection">
+      <Sticker
+        uri="./src/assets/images/stickers/sticker_thumbs-up.svg"
+        label="Thumbs Up"
+      />
+    </div>
+  );
 };
 
 export default TriggerSelection;
