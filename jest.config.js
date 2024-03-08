50290759448705f0080c9b54e0d8b7f0d1cca0bb
MauFrontier <maufrontier@gmail.com>
2024-03-07 20:34:13 -0600

    fix: Jest errors due to SVGR imports

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,8 @@ export default {
   },
   testMatch: ['<rootDir>/src/**/?(*.)+(spec|test).+(ts|tsx|js)'],
   moduleNameMapper: {
-    '\\.(css|scss|svg|png)$': '<rootDir>/src/mocks.tsx',
+    '\\.(css|scss|svg|png)$': '<rootDir>/src/utils/mocks/genericMock.tsx',
+    '\\.svg(\\?react)?$': '<rootDir>/src/utils/mocks/svgrMock.tsx', // Map SVG imports to the mock
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
 };
---
+++ src/utils/mocks/svgrMock.tsx
@@ -0,0 +1,2 @@
+export default 'div';
+export const ReactComponent = 'svg'; // Mock SVG as a simple "svg" string or a dummy React component
