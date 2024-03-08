0b022eac3318f3aba36e0125aada7a1724195c7f
MauFrontier <maufrontier@gmail.com>
2024-03-07 20:21:44 -0600

    ui: Style TargetingBuilder and Add Filter Group button

--- src/features/EmailMarketing/shared/TargetingBuilder/TargetingBuilder.tsx
+++ src/features/EmailMarketing/shared/TargetingBuilder/TargetingBuilder.tsx
@@ -1,19 +1,17 @@
 import Button from '../../../shared/ui/Button/Button';
-import Icon from '../../../shared/ui/Icon/Icon';
 import './TargetingBuilder.scss';
+import PlusCircleIcon from '@/assets/images/icons/icon_plus-circle-clear.svg?react';
 
 const addFilterGroup = () => {};
 
 const TargetingBuilder = () => {
   return (
-    <div
-      data-testid="TargetingBuilder"
-      className="targeting-builder-header mt-4 mb-2">
+    <div data-testid="TargetingBuilder" className="targeting-builder mt-4 mb-2">
       <Button
         onClick={addFilterGroup}
         label="Add Filter Group"
         className="w-full p-3">
-        <Icon uri="./src/assets/images/icons/icon_plus-circle-clear.svg" />
+        <PlusCircleIcon className="plus-circle-icon" />
         Add Filter Group
       </Button>
     </div>
