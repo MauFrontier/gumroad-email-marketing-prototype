7b65cd348c41bfe453da38434746bc22725b2747
MauFrontier <maufrontier@gmail.com>
2024-03-07 19:06:17 -0600

    ui: Migrate padding to utility class

--- src/features/EmailMarketing/shared/TargetingBuilderHeader/TargetingBuilderHeader.tsx
+++ src/features/EmailMarketing/shared/TargetingBuilderHeader/TargetingBuilderHeader.tsx
@@ -1,10 +1,20 @@
+import Button from '../../../shared/ui/Button/Button';
 import './TargetingBuilderHeader.scss';
+
+const openGenerateWithAIPanel = () => {};
 
 const TargetingBuilderHeader = () => {
   return (
     <div
       data-testid="TargetingBuilderHeader"
-      className="targeting-builder-header"></div>
+      className="targeting-builder-header mt-4 mb-2">
+      <p>
+        Recipients: <strong>102</strong>
+      </p>
+      <Button onClick={openGenerateWithAIPanel} label="Generate with AI">
+        Generate with AI
+      </Button>
+    </div>
   );
 };
 
