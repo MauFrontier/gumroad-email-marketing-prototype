dd5948edaf6e0b0bb4857d9685beffa381e0f4ef
MauFrontier <maufrontier@gmail.com>
2024-03-14 14:15:50 -0600

    test: Test that App is rendering EmailCampaignBuilder

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -6,3 +6,11 @@ it('renders App component', () => {
   const appComponent = screen.getByTestId('App');
   expect(appComponent).toBeInTheDocument();
 });
+
+it('renders EmailCampaignBuilder component', () => {
+  render(<App />);
+  const emailCampaignBuilderComponent = screen.getByLabelText(
+    'Email campaign builder',
+  );
+  expect(emailCampaignBuilderComponent).toBeInTheDocument();
+});
