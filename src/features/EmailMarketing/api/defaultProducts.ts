afb5ea20865cc4d317dcdab7dbcc8964496e7c5d
MauFrontier <maufrontier@gmail.com>
2024-03-14 14:06:02 -0600

    fix: Missing default products

---
+++ src/features/EmailMarketing/api/defaultProducts.ts
@@ -0,0 +1,34 @@
+export default [
+  {
+    key: 'brush-pack-1',
+    name: 'Brush pack 1',
+  },
+  {
+    key: 'christmas-bundle',
+    name: 'Christmas bundle',
+  },
+  {
+    key: 'illustration-tutorial',
+    name: 'Illustration Tutorial',
+  },
+  {
+    key: 'pro-video-course',
+    name: 'Pro video course',
+  },
+  {
+    key: 'tutorial-painting-101',
+    name: 'Tutorial: Painting 101',
+  },
+  {
+    key: 'tutorial-2024',
+    name: 'Tutorial 2024',
+  },
+  {
+    key: 'brush-pack-2024',
+    name: 'Brush pack 2024',
+  },
+  {
+    key: 'lut-pack-2023',
+    name: 'LUT pack 2023',
+  },
+];
