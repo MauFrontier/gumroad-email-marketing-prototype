6e808ec4c26e0594fe0ee1d5831f607c1eaa9b42
MauFrontier <maufrontier@gmail.com>
2024-03-16 02:14:05 -0600

    feat: Add aria-hidden to Sticker

--- src/features/shared/ui/Sticker/Sticker.tsx
+++ src/features/shared/ui/Sticker/Sticker.tsx
@@ -4,10 +4,20 @@ type Props = {
   uri?: string;
   label?: string;
   className?: string;
+  ariaHidden?: boolean;
 };
 
-const Sticker = React.memo(({uri, label, className}: Props) => {
-  return <img src={uri} alt={label} className={className} />;
-});
+const Sticker = React.memo(
+  ({uri, label, className, ariaHidden = true}: Props) => {
+    return (
+      <img
+        src={uri}
+        alt={label}
+        aria-hidden={ariaHidden}
+        className={className}
+      />
+    );
+  },
+);
 
 export default Sticker;
