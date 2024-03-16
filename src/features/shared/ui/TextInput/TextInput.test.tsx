c88d2ac7633781b1d5d09e43aef1582420ca1b7c
MauFrontier <maufrontier@gmail.com>
2024-03-16 02:21:31 -0600

    feat: Add aria-label, disabled, required, aria-invalid to TextInput

--- src/features/shared/ui/TextInput/TextInput.test.tsx
+++ src/features/shared/ui/TextInput/TextInput.test.tsx
@@ -10,6 +10,40 @@ describe('TextInput', () => {
     expect(screen.getByPlaceholderText('Enter text')).toBeInTheDocument();
   });
 
+  it('adds accessibility attributes', () => {
+    render(
+      <TextInput
+        label="Input 1"
+        invalid={true}
+        required={true}
+        disabled={true}
+      />,
+    );
+
+    const firstInput = screen.getByLabelText('Input 1');
+
+    expect(firstInput).toBeInTheDocument();
+    expect(firstInput).toHaveAttribute('aria-invalid', 'true');
+    expect(firstInput).toHaveAttribute('required', '');
+    expect(firstInput).toHaveAttribute('disabled', '');
+
+    render(
+      <TextInput
+        label="Input 2"
+        invalid={false}
+        required={false}
+        disabled={false}
+      />,
+    );
+
+    const secondInput = screen.getByLabelText('Input 2');
+
+    expect(secondInput).toBeInTheDocument();
+    expect(secondInput).toHaveAttribute('aria-invalid', 'false');
+    expect(secondInput).not.toHaveAttribute('required', '');
+    expect(secondInput).not.toHaveAttribute('disabled', '');
+  });
+
   it('calls onChange when input changes', async () => {
     const onChange = jest.fn();
     render(<TextInput onChange={onChange} />);
--- src/features/shared/ui/TextInput/TextInput.tsx
+++ src/features/shared/ui/TextInput/TextInput.tsx
@@ -1,17 +1,34 @@
 type Props = {
   id?: string;
   placeholder?: string;
+  label?: string;
   value?: string;
   onChange?: (value: string) => void;
+  invalid?: boolean;
+  disabled?: boolean;
+  required?: boolean;
 };
 
-const TextInput = ({id, placeholder, value, onChange}: Props) => {
+const TextInput = ({
+  id,
+  placeholder,
+  label,
+  value,
+  onChange,
+  invalid,
+  disabled,
+  required,
+}: Props) => {
   return (
     <input
       type="text"
       placeholder={placeholder}
+      aria-label={label || placeholder || ''}
       id={id}
       value={value}
+      disabled={disabled}
+      required={required}
+      aria-invalid={invalid ? 'true' : 'false'}
       onChange={e => onChange?.(e.target.value)}
     />
   );
