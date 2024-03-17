6d8d894a2671bf5df403d1a1157d4206b8f4c31f
MauFrontier <maufrontier@gmail.com>
2024-03-17 15:45:33 -0600

    chore: Mock uuid and store dispatch during tests

---
+++ src/utils/mocks/mocks.tsx
@@ -0,0 +1,12 @@
+jest.mock('uuid', () => ({
+  v4: () => 'EXAMPLE_UUID',
+}));
+
+export const mockDispatch = jest.fn();
+
+jest.mock('../../features/EmailMarketing/store/useEmailMarketingState', () => ({
+  useEmailMarketingState: () => ({
+    state: emailMarketingInitialState,
+    dispatch: mockDispatch,
+  }),
+}));
