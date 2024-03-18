a9f125a4d73256618237893f13eef8793fd2a119
MauFrontier <maufrontier@gmail.com>
2024-03-17 21:17:09 -0600

    feat: Implement ChatGPTService

---
+++ src/features/shared/ai/ChatGPTService.ts
@@ -0,0 +1,37 @@
+import {OpenAI} from 'openai';
+import {systemPrompt_generateTargeting} from '../../EmailMarketing/api/ai/generateTargeting/systemPrompt_generateTargeting';
+import {VITE_OPENAI_API_KEY} from '../../../constants';
+
+const apiKey = VITE_OPENAI_API_KEY;
+
+const openai = new OpenAI({
+  apiKey,
+  dangerouslyAllowBrowser: true,
+});
+
+export const SubmitAIPrompt = async (
+  prompt: string,
+): Promise<string | null> => {
+  try {
+    console.log('submitting prompt:', prompt);
+    const completion = await openai.chat.completions.create({
+      messages: [
+        {
+          role: 'system',
+          content: systemPrompt_generateTargeting,
+        },
+        {
+          role: 'user',
+          content: prompt,
+        },
+      ],
+      response_format: {type: 'json_object'},
+      model: 'gpt-4-1106-preview',
+    });
+    console.log('returned:', completion.choices[0].message.content);
+    return completion.choices[0].message.content;
+  } catch (error) {
+    console.error('ChatGPT API error:', error);
+    throw error;
+  }
+};
