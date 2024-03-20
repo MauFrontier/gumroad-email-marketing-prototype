93d19993a10592db871ba2f267908570ae020c34
MauFrontier <maufrontier@gmail.com>
2024-03-20 16:06:23 -0600

    feat: Implement current state viewer in InternalTools

--- src/features/EmailMarketing/shared/InternalTools/InternalTools.test.tsx
+++ src/features/EmailMarketing/shared/InternalTools/InternalTools.test.tsx
@@ -8,10 +8,11 @@ describe('InternalTools', () => {
     expect(screen.getByLabelText('Internal tools container')).toBeVisible();
   });
 
-  it('renders example prompts, and product editor', async () => {
+  it('renders example prompts, product editor, and current state', async () => {
     render(<InternalTools />);
 
     expect(screen.getByLabelText('Example prompts')).toBeVisible();
     expect(screen.getByLabelText('Edit products')).toBeVisible();
+    expect(screen.getByLabelText('Current state')).toBeVisible();
   });
 });
--- src/features/EmailMarketing/shared/InternalTools/InternalTools.tsx
+++ src/features/EmailMarketing/shared/InternalTools/InternalTools.tsx
@@ -1,9 +1,12 @@
+import {useEmailMarketingState} from '../../store/useEmailMarketingState';
 import './InternalTools.scss';
 import ExamplePrompts from './components/ExamplePrompts/ExamplePrompts';
 import examplePrompts from './components/ExamplePrompts/examplePromptsList';
 import ProductEditor from './components/ProductEditor/ProductEditor';
 
 const InternalTools = () => {
+  const {state} = useEmailMarketingState();
+
   return (
     <div aria-label="Internal tools container">
       <header>
@@ -13,6 +16,11 @@ const InternalTools = () => {
       <section role="region" aria-label="Internal tools">
         <ExamplePrompts examplePrompts={examplePrompts} />
         <ProductEditor />
+        <div aria-label="Current state">
+          <h3>Current state (readonly)</h3>
+          <pre className="code-block">{JSON.stringify(state, null, 2)}</pre>
+        </div>
+      </section>
     </div>
   );
 };
