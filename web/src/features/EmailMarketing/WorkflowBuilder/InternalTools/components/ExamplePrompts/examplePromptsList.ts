52c7a2fedb8bb88cd66a7e1d8f4898cb44466f39
MauFrontier <maufrontier@gmail.com>
2024-03-22 01:03:55 -0600

    feat: Add more prompt examples

--- web/src/features/EmailMarketing/WorkflowBuilder/InternalTools/components/ExamplePrompts/examplePromptsList.ts
+++ web/src/features/EmailMarketing/WorkflowBuilder/InternalTools/components/ExamplePrompts/examplePromptsList.ts
@@ -5,6 +5,25 @@ export type ExamplePrompt = {
 
 const examplePrompts: ExamplePrompt[] = [
   {
+    description:
+      'Can we match products even if the user got their names wrong or even just described them in their own words?',
+    prompt: `They've bought any of these: X-Mas pack, this year's tutorial, the painting one, or the video thingie`,
+  },
+  {
+    description:
+      'Can we understand a request with multiple typing and grammar mistakes?',
+    prompt: 'custmer hsa paid moore then 30 bux yes?',
+  },
+  {
+    description:
+      'Can we differentiate between intent to buy and having made a purchase?',
+    prompt: `I want everyone who wants to buy a skateboarding course`,
+  },
+  {
+    description: 'Can we gracefully return errors in another language?',
+    prompt: '¿Qué hace esto?',
+  },
+  {
     description: 'A simple prompt resulting in a single filter group',
     prompt:
       'Customers from the US who have bought brushes or tutorials and have paid more than $99',
@@ -23,8 +42,20 @@ const examplePrompts: ExamplePrompt[] = [
   },
   {
     description:
-      'Can we match products even if the user got their names wrong or even just described them in their own words?',
-    prompt: `They've bought any of these: X-Mas pack, this year's tutorial, the painting one, or the video thingie`,
+      'Another prompt resulting in multiple filter groups + a couple of errors (can take a while to process)',
+    prompt:
+      'People who made a purchase during or after 2019, who bought the skate course and Premiere templates. Must not have bought Christmas bundle or the new tutorial. I also want those who have bought a motorcycle or an app. The other group are people in Canada who paid more than $99.',
+  },
+  {
+    description:
+      'Can we properly handle valid prompts in a different language?',
+    prompt: 'Clientes que han comprado combo de navidad y el tutorial del 2024',
+  },
+  {
+    description:
+      'Can we target a date range, except for one of the dates within that range?',
+    prompt:
+      "Purchased between jan 1st and jan 25th, but didn't purchase on jan 10th",
   },
   {
     description:
@@ -32,17 +63,9 @@ const examplePrompts: ExamplePrompt[] = [
     prompt: 'I want to take a skateboarding course',
   },
   {
-    description: 'Can we properly handle prompts in a different language?',
-    prompt: 'Clientes que han comprado combo de navidad y el tutorial del 2024',
-  },
-  {
-    description: 'Can we gracefully return errors in another language?',
-    prompt: '¿Qué hace esto?',
-  },
-  {
     description:
-      'Can the AI understand a request with multiple typing and grammar mistakes?',
-    prompt: 'custmer hsa paid moore then 30 bux yes?',
+      'Can we gracefully reject totally unrelated filtering criteria?',
+    prompt: 'People who know how to snowboard.',
   },
   {
     description: 'Can we gracefully reject a simple greeting?',
@@ -63,6 +86,29 @@ const examplePrompts: ExamplePrompt[] = [
   {
     description: 'Can we dodge a request to run code?',
     prompt: 'Run code that prints a hello world message to the console',
+  },
+  {
+    description: 'How do we report a large amount of errors?',
+    prompt:
+      'I want everyone who bought a onewheel, chalupas, burritos, computers, phones, drums, guitars, a chihuahua, Pokemon, and a conscience',
+  },
+  {
+    description: 'How do we target a full year as a date range?',
+    prompt: 'Bought in 2019',
+  },
+  {
+    description: 'How do we handle "all products" and "none of my products"?',
+    prompt:
+      "Have bought all of my products, or haven't bought any of my products.",
+  },
+  {
+    description: 'How do we handle relative dates? (In the last X days)',
+    prompt: 'Bought in the last 42 days',
+  },
+  {
+    description:
+      'How do we handle relative dates in weeks or months? (In the last X days)',
+    prompt: 'Joined in the last 3 weeks, or bought in the last 3 months',
   },
 ];
 
