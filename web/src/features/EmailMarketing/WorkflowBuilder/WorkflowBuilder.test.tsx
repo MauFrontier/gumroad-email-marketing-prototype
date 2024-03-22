6e9ad0a937abb9da994b2802b51ae80b5915689d
MauFrontier <maufrontier@gmail.com>
2024-03-22 04:49:20 -0600

    feat: Add channel select and allowComments checkbox

--- web/src/features/EmailMarketing/WorkflowBuilder/WorkflowBuilder.test.tsx
+++ web/src/features/EmailMarketing/WorkflowBuilder/WorkflowBuilder.test.tsx
@@ -1,5 +1,9 @@
-import {render, screen} from '@testing-library/react';
+import {render, screen, within} from '@testing-library/react';
 import WorkflowBuilder from './WorkflowBuilder';
+import userEvent from '@testing-library/user-event';
+import {mockDispatch} from '../../../utils/mocks/mocks';
+import {EmailMarketingActionType} from '../store/emailMarketingActionTypes';
+import {Channels} from './emailMarketingTypes';
 
 describe('WorkflowBuilder', () => {
   it('renders component', () => {
@@ -27,5 +31,40 @@ describe('WorkflowBuilder', () => {
     expect(
       screen.getByLabelText('Internal tools container'),
     ).toBeInTheDocument();
+  });
+
+  it('renders channel selection', () => {
+    render(<WorkflowBuilder />);
+    expect(screen.getByLabelText('Select channel')).toBeInTheDocument();
+  });
+
+  it('sets the selected channel when a channel is selected', async () => {
+    render(<WorkflowBuilder />);
+    const selectContainer = screen.getByLabelText('Select channel');
+    const selectElement = within(selectContainer).getByRole('combobox');
+
+    await userEvent.selectOptions(selectElement, Channels.Email);
+
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: EmailMarketingActionType.SetChannel,
+      payload: Channels.Email,
+    });
+  });
+
+  it('renders allow comments checkbox', () => {
+    render(<WorkflowBuilder />);
+    expect(screen.getByLabelText('Allow comments')).toBeInTheDocument();
+  });
+
+  it('sets whether comments are allowed', async () => {
+    render(<WorkflowBuilder />);
+    const checkbox = screen.getByLabelText('Allow comments');
+
+    await userEvent.click(checkbox);
+
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: EmailMarketingActionType.SetAllowComments,
+      payload: false,
+    });
   });
 });
--- web/src/features/EmailMarketing/WorkflowBuilder/WorkflowBuilder.tsx
+++ web/src/features/EmailMarketing/WorkflowBuilder/WorkflowBuilder.tsx
@@ -2,8 +2,30 @@ import TextInput from '../../shared/ui/components/TextInput/TextInput';
 import InternalTools from './InternalTools/InternalTools';
 import TargetingBuilder from './TargetingBuilder/TargetingBuilder';
 import AudienceSelection from './AudienceSelection/AudienceSelection';
+import './WorkflowBuilder.scss';
+import {Channels} from './emailMarketingTypes';
+import {EmailMarketingActionType} from '../store/emailMarketingActionTypes';
+import {useEmailMarketingState} from '../store/useEmailMarketingState';
 
 const WorkflowBuilder = () => {
+  const {state, dispatch} = useEmailMarketingState();
+
+  const handleChangeChannel = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch({
+      type: EmailMarketingActionType.SetChannel,
+      payload: event.target.value as Channels,
+    });
+  };
+
+  const handleAllowCommentsChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    dispatch({
+      type: EmailMarketingActionType.SetAllowComments,
+      payload: event.target.checked,
+    });
+  };
+
   return (
     <form aria-label="Workflow builder">
       <label>
@@ -12,6 +34,23 @@ const WorkflowBuilder = () => {
       </label>
       <AudienceSelection />
       <TargetingBuilder />
+      <label aria-label="Select channel">
+        Channel
+        <select onChange={handleChangeChannel} value={state.channel}>
+          <option value={Channels.EmailAndProfile}>
+            Send email and post to my profile
+          </option>
+          <option value={Channels.Email}>Send email</option>
+        </select>
+      </label>
+      <label aria-label="Allow comments">
+        <input
+          type="checkbox"
+          onChange={handleAllowCommentsChange}
+          checked={state.allowComments}
+        />
+        Allow comments in your posts
+      </label>
       <InternalTools />
     </form>
   );
